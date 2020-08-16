7f88a9fb01abad7b8f7bab25eac143bac6274c04
Shameer <mshameer237@gmail.com>
2020-08-16 16:02:55 +0530

    major commit
    removed sites folder
    updated skill tab
    add github links

--- sites/NexGenSm/js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('.na-toggle, .ham-menu nav').click(function(){
-    $('.na-toggle').toggleClass('navbar-on');
-    $('.ham-menu nav').fadeToggle();
-    $('.ham-menu nav').removeClass('nav-hide');
-  });
-});
